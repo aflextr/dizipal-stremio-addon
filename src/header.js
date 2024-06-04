8c3367eef848e9320cee9f4b9fb09f4fc70d7afc
aflextr <eyup.elitass@gmail.com>
2024-06-04 22:08:21 +0300

    dizipal adress changed

--- src/header.js
+++ src/header.js
@@ -4,7 +4,7 @@ const header = {
     "Accept-Language":"tr,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
     "Sec-Ch-Ua-Platform":"Windows",
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
-    "Cookie":"dizipal735_com=%7B%22HttpHost%22%3A%22dizipal735.com%22%2C%22Protokol%22%3A%22https%22%2C%22Port%22%3A443%2C%22KulAdSifre%22%3Anull%2C%22UrlAdresi%22%3A%22%5C%2F%22%2C%22GetVeri%22%3Anull%2C%22GitOpjeId%22%3Anull%2C%22DnsAdresi%22%3A%220%22%2C%22URL_Adresi%22%3A%22https%3A%5C%2F%5C%2Fdizipal735.com%5C%2F%22%2C%22GirisIP%22%3A%22188.114.96.7%22%7D",
+    "Cookie":"dizipal736_com=%7B%22HttpHost%22%3A%22dizipal736.com%22%2C%22Protokol%22%3A%22https%22%2C%22Port%22%3A443%2C%22KulAdSifre%22%3Anull%2C%22UrlAdresi%22%3A%22%5C%2F%22%2C%22GetVeri%22%3Anull%2C%22GitOpjeId%22%3Anull%2C%22DnsAdresi%22%3A0%2C%22URL_Adresi%22%3A%22https%3A%5C%2F%5C%2Fdizipal736.com%5C%2F%22%2C%22GirisIP%22%3A%22188.114.97.7%22%7D",
 
    "Origin":process.env.PROXY_URL,
 
