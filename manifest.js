c72daf8da10e13242259c73053d884ea3fe5c83f
aflextr <eyup.elitass@gmail.com>
2024-08-02 21:01:29 +0300

    test

--- manifest.js
+++ manifest.js
@@ -1,12 +1,9 @@
 require("dotenv").config({path:"./.env"})
 const manifest = {
-
     id: 'org.dizipal-stremio-addon',
     version: '1.0.1',
-
     name: 'Dizipal',
     description: "Dizipal'den dizi ve filmleri stremionuza getirir.  Proxy Kullandığımız için eklenti yavaş çalışabilir.",
-
     contactEmail: "eyup.elitass@gmail.com"+ "<p><a target='_blank' href='https://github.com/aflextr/dizipal-stremio-addon'>GitHub</a></p>",
     logo: `${process.env.HOSTING_URL}/images/dizipal.jpg`,
     background: `${process.env.HOSTING_URL}/images/background.jpg`,
