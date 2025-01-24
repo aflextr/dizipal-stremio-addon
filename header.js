880f63b4293894dc2ec2575ba39c8b5494cf912f
aflextr <eyup.elitass@gmail.com>
2025-01-24 17:26:13 +0300

    update

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const scrapeCookie = require("./src/scrapeProxyCookie");
 scrapeCookie.fetchWithCookies(process.env.PROXY_URL).then((value)=>{
-    if (value) {
+    if (value.length > 10) {
         header.Cookie = value;
     }
 })
@@ -10,7 +10,7 @@ const header = {
     "Accept-Language":"tr,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
     "Sec-Ch-Ua-Platform":"Windows",
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
-    "Cookie":"",
+    "Cookie":"dizipal871_com=%7B%22HttpHost%22%3A%22dizipal871.com%22%2C%22Protokol%22%3A%22https%22%2C%22Port%22%3A443%2C%22KulAdSifre%22%3Anull%2C%22UrlAdresi%22%3A%22%5C%2F%22%2C%22GetVeri%22%3Anull%2C%22GitOpjeId%22%3Anull%2C%22DnsAdresi%22%3A0%2C%22URL_Adresi%22%3A%22https%3A%5C%2F%5C%2Fdizipal871.com%5C%2F%22%2C%22GirisIP%22%3A%22104.21.23.10%22%7D",
     "Origin":process.env.PROXY_URL,
     "Referer":process.env.PROXY_URL,
 }
